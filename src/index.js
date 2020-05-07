df4de3a2fd09c51e7c6cca032fe946712bb65c22
Jonah Ullman <jonahmullman@gmail.com>
2020-05-07 11:42:17 -0500

    added initial firebase/firestore config, set users on state of app component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyD1vMToUD8Mqh2e75udVVUQeat1pMkiWd4',
+  authDomain: 'yellow-proof-of-concept.firebaseapp.com',
+  databaseURL: 'https://yellow-proof-of-concept.firebaseio.com',
+  projectId: 'yellow-proof-of-concept',
+  storageBucket: 'yellow-proof-of-concept.appspot.com',
+  messagingSenderId: '824038108224',
+  appId: '1:824038108224:web:8294ca4c0460b1b77843a5',
+  measurementId: 'G-DJGLCMY3KZ',
+};
+
+export const fire = firebase.initializeApp(config);
+
+export const db = firebase.firestore();
 
 ReactDOM.render(
   <React.StrictMode>
